1eb90fc2cde7e81e638b3949ecf27c31b352e3cd
faisal-khan12 <sahirfls@gmail.com>
2021-08-21 16:40:24 +0500

    Photo Library done

---
+++ app/src/main/java/com/dotaustere/photoeditorpro/ImageSaveActivity.java
@@ -0,0 +1,14 @@
+package com.dotaustere.photoeditorpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageSaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_save);
+    }
+}
--- app/src/main/java/com/dotaustere/photoeditorpro/ui/home/HomeFragment.java
+++ app/src/main/java/com/dotaustere/photoeditorpro/ui/home/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.dotaustere.photoeditorpro.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +17,57 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.dotaustere.photoeditorpro.R;
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    private HomeViewModel homeViewModel;
+    private ImageView gallery;
+    int IMAGE_REQUEST_CODE = 45;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        gallery = root.findViewById(R.id.gellery_img);
+        gallery.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
             }
         });
+
+
         return root;
+
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_REQUEST_CODE) {
+            if (data.getData() != null) {
+                Uri filepath = data.getData();
+                Intent dsPhotoEditorIntent = new Intent(getActivity(), DsPhotoEditorActivity.class);
+                dsPhotoEditorIntent.setData(filepath);
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "PhotoEditorPro");
+                int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+                startActivityForResult(dsPhotoEditorIntent, 200);
+            }
+        }
+
+        if (requestCode == 200){
+
+        }
+    }
+
+
 }
