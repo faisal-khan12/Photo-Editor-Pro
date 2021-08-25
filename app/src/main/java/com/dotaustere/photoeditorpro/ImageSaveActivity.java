3822d82ed3aa886158ed49018d62276ff5f7599c
faisal-khan12 <sahirfls@gmail.com>
2021-08-25 15:40:47 +0500

    camera working completed

--- app/src/main/java/com/dotaustere/photoeditorpro/ImageSaveActivity.java
+++ app/src/main/java/com/dotaustere/photoeditorpro/ImageSaveActivity.java
@@ -2,13 +2,33 @@ package com.dotaustere.photoeditorpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.dotaustere.photoeditorpro.ui.home.HomeFragment;
 
 public class ImageSaveActivity extends AppCompatActivity {
+
+    private  ImageView backBn,home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_save);
+        getSupportActionBar().hide();
+        ImageView saveImg = findViewById(R.id.saveImg);
+        saveImg.setImageURI(getIntent().getData());
+
+
+        backBn = findViewById(R.id.backBtn);
+        backBn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/dotaustere/photoeditorpro/ui/home/HomeFragment.java
+++ app/src/main/java/com/dotaustere/photoeditorpro/ui/home/HomeFragment.java
@@ -1,8 +1,13 @@
 package com.dotaustere.photoeditorpro.ui.home;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,20 +17,27 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.dotaustere.photoeditorpro.ImageSaveActivity;
 import com.dotaustere.photoeditorpro.R;
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
 import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+
+import java.io.ByteArrayOutputStream;
 
 public class HomeFragment extends Fragment {
 
 
     private HomeViewModel homeViewModel;
     private ImageView gallery;
+    private ImageView cameraImg;
     int IMAGE_REQUEST_CODE = 45;
+    int CAMERA_REQUEST_CODE = 14;
+    int RESULT_CODE = 200;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +56,37 @@ public class HomeFragment extends Fragment {
         });
 
 
+        cameraImg = root.findViewById(R.id.camera_img);
+        cameraImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.CAMERA},32);
+
+                }else {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent,CAMERA_REQUEST_CODE);
+
+                }
+
+            }
+
+        });
+
+
+
+
+
+
+
+
         return root;
+
+
+
+
 
 
     }
@@ -60,14 +102,54 @@ public class HomeFragment extends Fragment {
                 dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "PhotoEditorPro");
                 int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
                 dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
-                startActivityForResult(dsPhotoEditorIntent, 200);
+                startActivityForResult(dsPhotoEditorIntent, RESULT_CODE);
             }
         }
 
-        if (requestCode == 200){
+        if (requestCode == RESULT_CODE){
+            Intent intent = new Intent(getActivity(), ImageSaveActivity.class);
+            intent.setData(data.getData());
+            startActivity(intent);
 
         }
+
+        if (requestCode == CAMERA_REQUEST_CODE){
+
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Uri uri = getImageUri(photo);
+            Intent dsPhotoEditorIntent = new Intent(getActivity(), DsPhotoEditorActivity.class);
+            dsPhotoEditorIntent.setData(uri);
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "PhotoEditorPro");
+            int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+            startActivityForResult(dsPhotoEditorIntent, RESULT_CODE);
+
+
+
+            
+            
+        }
+
+
+
+    }
+
+    public Uri getImageUri(Bitmap bitmap){
+
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,arrayOutputStream);
+        String path = MediaStore.Images.Media.insertImage(getContext().getContentResolver(),bitmap,"Title",null);
+        return Uri.parse(path);
+
     }
 
 
+
 }
+
+
+
+
+
+
+
